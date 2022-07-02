d03078f28a957055a7eb953bcdebecbd585a3071
Adithya Krishna <adikrish@redhat.com>
2022-07-02 14:08:24 +0530

    Updated Desc
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Adithya Krishna',
     description:
-      'Adithya Krishna is a Software Engineer based out of Bengaluru, IN.',
+      'Adithya Krishna is a Software Engineer based out of Bengaluru, India.',
     siteUrl: 'https://adithyaakrishna.github.io', // No trailing slash allowed!
     image: '/Adi.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@adii_kris',
