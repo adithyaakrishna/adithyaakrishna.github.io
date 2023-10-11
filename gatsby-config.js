210fe590fb7088c9f7f3b7b1504c5c2b955f4ce8
Adithya Krishna <aadithya794@gmail.com>
2023-10-11 20:44:41 +0530

    Updated Images and Favicon
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/logo.png',
+        icon: 'src/images/Firstbump.png',
       },
     },
     `gatsby-plugin-offline`,
