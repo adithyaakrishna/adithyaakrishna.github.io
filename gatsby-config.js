06caa0904731b95b9ae61fb430b65f5e4b24440e
Adithya Krishna <aadithya794@gmail.com>
2023-10-11 20:48:13 +0530

    Added Favicon Name
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/Firstbump.png',
+        icon: 'src/images/Fistbump.png',
       },
     },
     `gatsby-plugin-offline`,
