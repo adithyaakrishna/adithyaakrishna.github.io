e3f1c8074ce595f03ae9bb2ff4819265e31e4d79
Adithya Krishna <23498248+adithyaakrishna@users.noreply.github.com>
2026-03-03 14:34:40 +0000

    GitHub Pages Auto Deploy

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9d21d4da316d64f3836d.js"
+    "url": "webpack-runtime-311df19ce4a6dc06a4a1.js"
   },
   {
     "url": "framework-9c9e72562348597eca86.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "262fb9e39380b6de5df356ec0dfb5af7"
+    "revision": "8aa026c4e13b94485209539f7d6e6730"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e1b95d697ef69d0a253f.js"
